20e60bf89e03a63fd1766c3a4825b3a75516dbf1
Agung Wiranata <agungwiranata02@gmail.com>
2024-11-26 11:58:40 +0700

    improvement

--- environments/development/outputs.tf
+++ environments/development/outputs.tf
@@ -1,13 +1,3 @@
-output "cloudwatch_alarms" {
-  value = [
-    aws_cloudwatch_metric_alarm.cpu_high.alarm_name,
-    aws_cloudwatch_metric_alarm.memory_high.alarm_name,
-    aws_cloudwatch_metric_alarm.status_check_failed.alarm_name,
-    aws_cloudwatch_metric_alarm.network_in_high.alarm_name,
-    aws_cloudwatch_metric_alarm.network_out_high.alarm_name
-  ]
-}
-
 output "vpc_id" {
   description = "VPC ID of the created VPC"
   value       = module.vpc.vpc_id
