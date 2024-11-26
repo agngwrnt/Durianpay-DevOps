8f5f11dacd2af5ec96bb60a7987bbc9a25341772
Agung Wiranata <agungwiranata02@gmail.com>
2024-11-26 12:07:16 +0700

    improvement

--- modules/cloudwatch/outputs.tf
+++ modules/cloudwatch/outputs.tf
@@ -1,9 +1,19 @@
-output "cloudwatch_alarms" {
-  value = [
-    aws_cloudwatch_metric_alarm.cpu_high.alarm_name,
-    aws_cloudwatch_metric_alarm.memory_high.alarm_name,
-    aws_cloudwatch_metric_alarm.status_check_failed.alarm_name,
-    aws_cloudwatch_metric_alarm.network_in_high.alarm_name,
-    aws_cloudwatch_metric_alarm.network_out_high.alarm_name
-  ]
+output "cpu_high_alarm_name" {
+  value = aws_cloudwatch_metric_alarm.cpu_high_alarm.name
+}
+
+output "memory_high_alarm_name" {
+  value = aws_cloudwatch_metric_alarm.memory_high_alarm.name
+}
+
+output "status_check_failed_alarm_name" {
+  value = aws_cloudwatch_metric_alarm.status_check_failed_alarm.name
+}
+
+output "network_in_high_alarm_name" {
+  value = aws_cloudwatch_metric_alarm.network_in_high_alarm.name
+}
+
+output "network_out_high_alarm_name" {
+  value = aws_cloudwatch_metric_alarm.network_out_high_alarm.name
 }
